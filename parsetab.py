6355607f5bc75c4b0bcf129dc98061113c4530f2
Daniel <danielson@college.harvard.edu>
2024-12-11 06:17:03 +0100

    changes to primitives

--- parsetab.py
+++ parsetab.py
@@ -6,9 +6,9 @@ _tabversion = '3.10'
 
 _lr_method = 'LALR'
 
-_lr_signature = 'sexpr_listDEFINE ID LPAREN NUMBER RPARENsexpr_list : sexpr\n                  | sexpr_list sexprsexpr : LPAREN ID param_list RPAREN\n             | define_exprdefine_expr : LPAREN DEFINE ID param_list LPAREN sexpr_list RPAREN RPARENparam_list : param\n                  | param_list paramparam : LPAREN ID NUMBER RPAREN'
+_lr_signature = 'sexpr_listBOOLEAN DEFINE ID LPAREN NUMBER RPAREN STRINGsexpr_list : sexpr\n| sexpr_list sexprsexpr : LPAREN ID param_list RPAREN\n| define_exprdefine_expr : LPAREN DEFINE ID param_list LPAREN sexpr_list RPAREN RPARENparam_list : param\n| param_list paramparam : LPAREN ID NUMBER RPAREN\n| LPAREN ID STRING RPAREN\n| LPAREN ID BOOLEAN RPAREN'
     
-_lr_action_items = {'LPAREN':([0,1,2,4,5,6,9,10,11,13,14,15,17,18,19,21,],[3,3,-1,-4,-2,8,8,-6,8,-3,-7,17,3,-8,3,-5,]),'$end':([1,2,4,5,13,21,],[0,-1,-4,-2,-3,-5,]),'RPAREN':([2,4,5,9,10,13,14,16,18,19,20,21,],[-1,-4,-2,13,-6,-3,-7,18,-8,20,21,-5,]),'ID':([3,7,8,17,],[6,11,12,12,]),'DEFINE':([3,],[7,]),'NUMBER':([12,],[16,]),}
+_lr_action_items = {'LPAREN':([0,1,2,4,5,6,9,10,11,13,14,15,19,20,21,22,23,25,],[3,3,-1,-4,-2,8,8,-6,8,-3,-7,19,3,-8,-9,-10,3,-5,]),'$end':([1,2,4,5,13,25,],[0,-1,-4,-2,-3,-5,]),'RPAREN':([2,4,5,9,10,13,14,16,17,18,20,21,22,23,24,25,],[-1,-4,-2,13,-6,-3,-7,20,21,22,-8,-9,-10,24,25,-5,]),'ID':([3,7,8,19,],[6,11,12,12,]),'DEFINE':([3,],[7,]),'NUMBER':([12,],[16,]),'STRING':([12,],[17,]),'BOOLEAN':([12,],[18,]),}
 
 _lr_action = {}
 for _k, _v in _lr_action_items.items():
@@ -17,7 +17,7 @@ for _k, _v in _lr_action_items.items():
       _lr_action[_x][_k] = _y
 del _lr_action_items
 
-_lr_goto_items = {'sexpr_list':([0,17,],[1,19,]),'sexpr':([0,1,17,19,],[2,5,2,5,]),'define_expr':([0,1,17,19,],[4,4,4,4,]),'param_list':([6,11,],[9,15,]),'param':([6,9,11,15,],[10,14,10,14,]),}
+_lr_goto_items = {'sexpr_list':([0,19,],[1,23,]),'sexpr':([0,1,19,23,],[2,5,2,5,]),'define_expr':([0,1,19,23,],[4,4,4,4,]),'param_list':([6,11,],[9,15,]),'param':([6,9,11,15,],[10,14,10,14,]),}
 
 _lr_goto = {}
 for _k, _v in _lr_goto_items.items():
@@ -27,12 +27,14 @@ for _k, _v in _lr_goto_items.items():
 del _lr_goto_items
 _lr_productions = [
   ("S' -> sexpr_list","S'",1,None,None,None),
-  ('sexpr_list -> sexpr','sexpr_list',1,'p_sexpr_list','interpret.py',38),
-  ('sexpr_list -> sexpr_list sexpr','sexpr_list',2,'p_sexpr_list','interpret.py',39),
-  ('sexpr -> LPAREN ID param_list RPAREN','sexpr',4,'p_sexpr','interpret.py',46),
-  ('sexpr -> define_expr','sexpr',1,'p_sexpr','interpret.py',47),
-  ('define_expr -> LPAREN DEFINE ID param_list LPAREN sexpr_list RPAREN RPAREN','define_expr',8,'p_define_expr','interpret.py',54),
-  ('param_list -> param','param_list',1,'p_param_list','interpret.py',62),
-  ('param_list -> param_list param','param_list',2,'p_param_list','interpret.py',63),
-  ('param -> LPAREN ID NUMBER RPAREN','param',4,'p_param','interpret.py',70),
+  ('sexpr_list -> sexpr','sexpr_list',1,'p_sexpr_list','interpret.py',50),
+  ('sexpr_list -> sexpr_list sexpr','sexpr_list',2,'p_sexpr_list','interpret.py',51),
+  ('sexpr -> LPAREN ID param_list RPAREN','sexpr',4,'p_sexpr','interpret.py',58),
+  ('sexpr -> define_expr','sexpr',1,'p_sexpr','interpret.py',59),
+  ('define_expr -> LPAREN DEFINE ID param_list LPAREN sexpr_list RPAREN RPAREN','define_expr',8,'p_define_expr','interpret.py',66),
+  ('param_list -> param','param_list',1,'p_param_list','interpret.py',74),
+  ('param_list -> param_list param','param_list',2,'p_param_list','interpret.py',75),
+  ('param -> LPAREN ID NUMBER RPAREN','param',4,'p_param','interpret.py',82),
+  ('param -> LPAREN ID STRING RPAREN','param',4,'p_param','interpret.py',83),
+  ('param -> LPAREN ID BOOLEAN RPAREN','param',4,'p_param','interpret.py',84),
 ]
--- render.py
+++ render.py
@@ -5,19 +5,20 @@ def render_canvas(shapes, width=500, height=500, filename="output.png"):
     draw = ImageDraw.Draw(image)
 
     for shape in shapes:
+        print("render", shape, shape.color, shape.fill)
         shape.render(draw)
 
     image.save(filename)
     print(f"Canvas saved as {filename}")
 
 def render_animation(frames, width=500, height=500, prefix="frame"):
-    for idx, shapes in enumerate(frames):
+    for idx, frame_shapes in enumerate(frames):
         filename = f"{prefix}_{idx:03}.png"
         image = Image.new("RGB", (width, height), "white")
         draw = ImageDraw.Draw(image)
-        
-        for shape in shapes:
-            shape.render(draw)
-        
+
+        for shape in frame_shapes:
+            shape.render(draw)  # Use shape objects directly
+
         image.save(filename)
         print(f"Frame {idx} saved as {filename}")
